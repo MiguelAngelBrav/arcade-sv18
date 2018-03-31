067e8888e1f131dd8a2c081384061c11ee8a74e1
svalls <34880594+svalls@users.noreply.github.com>
2018-03-31 12:15:17 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 // ENEMY CLASS
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-
-    // 1 Loading the image by setting this.sprite to the appropriate image in the image folder 
-    this.sprite = 'images/enemy-bug.png';
-
-    // 2 Setting the Enemy initial location
+    // 1 Setting the Enemy initial location
     this.x = x;
     this.y = y;
-
-    // 3 Setting the Enemy speed
+    // 2 Setting the Enemy speed
+    this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    // 3 Loading the image by setting this.sprite to the appropriate image in the image folder 
+    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -23,6 +20,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // in this method you will change the x position of enemies
+    // and yeah here you will need to multiply dt to speed
+    // this.speed * dt;
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,33 +32,43 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 //PLAYER CLASS
 // This class requires an update(), render() and a handleInput() method.
-var Player = function() {
-    // 1 Loading the image by setting this.sprite to the appropriate image in the image folder
-    this.sprite = 'images/char-pink-girl.png';
-
-    // 2 Setting the Player initial location
+//var Player = function(x, y, speed???) {
+var Player = function(x, y) {
+    // Setting the Player initial location
     this.x = x;
     this.y = y;
-
+    // Setting the Player speed
+    // this.speed = speed;
+    // Loading the image by setting this.sprite to the appropriate image in the image folder
+    this.sprite = 'images/char-pink-girl.png';
 };
 
-// 3 The update method for the Player (can be similar to the one for the Enemy)
-//update() method
+// 1 The update method for the Player (can be similar to the one for the Enemy)
+Player.prototype.update = function(dt) {
+    // do something
+};
 
-// 4 The render method for the Player (use the code from the render method for the Enemy)
+// 2 The render method for the Player (use the code from the render method for the Enemy)
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// 5 The handleInput method
-//Player.handleInput() method
+// 3 The handleInput method - Player.handleInput() method
+Player.prototype.handleInput = function() {
+    // do something
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
+var player = new Player();
+
 
 
 // This listens for key presses and sends the keys to your
